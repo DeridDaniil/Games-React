a5149d12d710fd2d28e8a665d5db0c9658c4ef05
Derid Daniil <deriddaniil@gmail.com>
2025-01-24 19:50:48 +0500

    fetch: added queen and bishop movement in chess

--- src/Games/Chess/arbiter/arbiter.js
+++ src/Games/Chess/arbiter/arbiter.js
@@ -1,9 +1,11 @@
-import { getKnightMoves, getRookMoves } from "./getMoves"
+import { getBishopMoves, getKnightMoves, getQueenMoves, getRookMoves } from "./getMoves"
 
 const arbiter = {
   getRegularMoves: function ({ position, figure, axisY, axisX }) {
     if (figure.slice(6) === 'rook') return getRookMoves({ position, figure, axisY, axisX });
     if (figure.slice(6) === 'knight') return getKnightMoves({ position, figure, axisY, axisX });
+    if (figure.slice(6) === 'bishop') return getBishopMoves({ position, figure, axisY, axisX });
+    if (figure.slice(6) === 'queen') return getQueenMoves({ position, figure, axisY, axisX });
   }
 }
 
--- src/Games/Chess/arbiter/getMoves.js
+++ src/Games/Chess/arbiter/getMoves.js
@@ -50,3 +50,39 @@ export const getKnightMoves = ({ position, figure, axisY, axisX }) => {
 
   return moves;
 }
+
+export const getBishopMoves = ({ position, figure, axisY, axisX }) => {
+  const moves = [];
+  const us = figure.slice(0, 5);
+  const enemy = us === 'white' ? 'black' : 'white';
+  const directory = [
+    [-1, 1],
+    [-1, -1],
+    [1, -1],
+    [1, 1]
+  ];
+
+  directory.forEach(dir => {
+    for (let i = 1; i < 8; i++) {
+      const y = axisY + (i * dir[0]);
+      const x = axisX + (i * dir[1]);
+      if (position?.[y]?.[x] === undefined) break;
+      if (position[y][x].slice(0, 5) === us) break;
+      if (position[y][x].slice(0, 5) === enemy) {
+        moves.push([y, x]);
+        break;
+      };
+      moves.push([y, x]);
+    };
+  });
+
+  return moves;
+}
+
+export const getQueenMoves = ({ position, figure, axisY, axisX }) => {
+  const moves = [
+    ...getRookMoves({ position, figure, axisY, axisX }),
+    ...getBishopMoves({ position, figure, axisY, axisX })
+  ];
+  return moves;
+}
