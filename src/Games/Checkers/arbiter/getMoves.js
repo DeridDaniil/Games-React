143d4d1898c36c753890cbae5ed80193ebfc7322
Derid Daniil <deriddaniil@gmail.com>
2025-02-19 23:09:20 +0300

    fetch: added the ability to attack backward for checkers

--- src/Games/Checkers/arbiter/getMoves.js
+++ src/Games/Checkers/arbiter/getMoves.js
@@ -1,17 +1,20 @@
 export const getCheckerMoves = ({ position, checker, axisY, axisX }) => {
   const moves = [];
   const isWhite = checker.slice(0, 5) === 'white';
+  const attackY = [1, -1];
   const moveX = [1, -1];
   const y = isWhite ? 1 : - 1;
   const enemy = isWhite ? 'black' : 'white';
 
-  moveX.forEach(x => {
-    if (position[axisY + y][axisX + x] !== undefined && position[axisY + y][axisX + x].slice(0, 5) === enemy) {
-      if (position[axisY + y + y][axisX + x + x] !== undefined && !position[axisY + y + y][axisX + x + x]) {
-        moves.push([axisY + y + y, axisX + x + x]);
+  attackY.forEach(y => {
+    moveX.forEach(x => {
+      if (position[axisY + y][axisX + x] !== undefined && position[axisY + y][axisX + x].slice(0, 5) === enemy) {
+        if (position[axisY + y + y][axisX + x + x] !== undefined && !position[axisY + y + y][axisX + x + x]) {
+          moves.push([axisY + y + y, axisX + x + x]);
+        }
       }
-    }
-  });
+    })
+  })
 
   if (moves.length > 0) return moves;
 
@@ -25,16 +28,17 @@ export const getCheckerMoves = ({ position, checker, axisY, axisX }) => {
 }
 
 export const getCheckerAttack = ({ position, checker, axisY, axisX }) => {
-  let moves = [];
-  const isWhite = checker.slice(0, 5) === 'white';
-  const y = isWhite ? 1 : -1;
+  const moves = [];
+  const attackY = [1, -1];
   const moveX = [1, -1];
-  const enemy = isWhite ? 'black' : 'white';
+  const enemy = checker.slice(0, 5) === 'white' ? 'black' : 'white';
 
-  moveX.forEach(x => {
-    if (position[axisY + y][axisX + x] !== undefined && position[axisY + y][axisX + x].slice(0, 5) === enemy) {
-      moves.push([axisY + y, axisX + x])
-    }
+  attackY.forEach(y => {
+    moveX.forEach(x => {
+      if (position[axisY + y][axisX + x] !== undefined && position[axisY + y][axisX + x].slice(0, 5) === enemy) {
+        moves.push([axisY + y, axisX + x])
+      }
+    })
   })
 
   return moves;
